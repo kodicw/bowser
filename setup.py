8e9b318e48d678f43823d88aeb51e4b775a92c2c
Kodi Walls <kodi.walls@smartcommunications.us>
2025-04-30 12:41:02 -0700

    ...

--- setup.py
+++ setup.py
@@ -5,11 +5,6 @@ setup(
     name="bowser",
     description="bowser",
     packages=find_packages(),
-    # entry_points={
-    #     "console_scripts": [
-    #         "pfbrowser = pfbrowser:cli",
-    #     ]
-    # },
     install_requires=[
         "selenium==4.27.1",
     ],
